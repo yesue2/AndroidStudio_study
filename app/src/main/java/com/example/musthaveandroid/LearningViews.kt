b10df4fcdb60aa62d9f6490ad339dc4cac722f57
YeSeul <ysbjh2464@naver.com>
2024-01-21 01:49:42 +0900

    feat: add view activity, xml files

---
+++ app/src/main/java/com/example/musthaveandroid/LearningViews.kt
@@ -0,0 +1,11 @@
+package com.example.musthaveandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LearningViews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learning_views)
+    }
+}
--- app/src/main/java/com/example/musthaveandroid/MainActivity.kt
+++ app/src/main/java/com/example/musthaveandroid/MainActivity.kt
@@ -16,8 +16,9 @@ class MainActivity : ComponentActivity() {
         //findViewById()함수는 뷰의 id값을 인수로 받음
         val sub_button = findViewById<Button>(R.id.sub_button)
         val two_button = findViewById<Button>(R.id.two_button)
+        val ailen_button = findViewById<Button>(R.id.ailen_button)
 
-        //버튼일 클릭되었을 때 행동을 SetOnClickListener()에 지정
+        //버튼이 클릭되었을 때 행동을 SetOnClickListener()에 지정
         sub_button.setOnClickListener {
             //intent 변수 생성 후 인턴드 객체 생성
             //Intent(현재 객체, 이동할 목적지)
@@ -28,6 +29,11 @@ class MainActivity : ComponentActivity() {
 
         two_button.setOnClickListener {
             val intent = Intent(this, TwoColorActivity::class.java)
+            startActivity(intent)
+        }
+
+        ailen_button.setOnClickListener {
+            val intent = Intent(this, LearningViews::class.java)
             startActivity(intent)
         }
     }
